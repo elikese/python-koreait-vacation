1de741dabdc754eaae2fc9572d2387789513f9ee
elikese <elikese@gmail.com>
2026-02-25 10:33:29 +0900

    feat: 크롤링 완료

--- day20/네이버웹툰.py
+++ day20/네이버웹툰.py
@@ -44,6 +44,59 @@ menu_bar = driver.find_element(By.CSS_SELECTOR, "#wrap > header > div.SubNavigat
 menus = menu_bar.find_elements(By.CSS_SELECTOR, "li > a")
 menus = menus[1:8] # 월 ~ 일 필터링
 
+"""
+[
+    {
+        "day_of_week": "월",
+        "webtoon_items": [{},{}...{}] 웹툰 하나당 dict 하나
+    },
+    {},
+    ...
+    {} 요일하나당 dict 하나
+] 전체를 저장하는 list
+"""
+webtoon_list = [] # 전체를 저장하는 list
 for menu in menus:
-    print(menu.text) # 월 ~ 일
+    menu.click() # 월~일 탭 페이지 이동
+    sleep(1.5)
+
+    webtoon_dict = {
+        "day_of_week": menu.text,
+        "webtoon_items": [] # 웹툰 하나당 {}하나 넣을거임!
+    }
+
+    webtoon_items = driver.find_elements(By.CSS_SELECTOR, "#content > div:nth-child(1) > ul > li")
+    print(len(webtoon_items))
+    for item in webtoon_items:
+        scroll_to(item) # 스크롤
+
+        # 제목
+        title = item.find_element(By.CLASS_NAME, "ContentTitle__title--e3qXt").text
+        # 작가
+        author = item.find_element(By.CLASS_NAME, "ContentAuthor__author--CTAAP").text
+        # 별점
+        rating = item.find_element(By.CLASS_NAME, "rating_area").text
+        rating = rating[2:].strip()
+        # 썸네일 - img태그
+        img_url = item.find_element(By.CLASS_NAME, "Poster__image--d9XTI").get_attribute("src")
+
+        print(f"제목: {title}")
+        print(f"작가: {author}")
+        print(f"평점: {rating}")
+        print(f"이미지url: {img_url}")
+
+        webtoon_dict["webtoon_items"].append({
+            "title": title,
+            "author": author,
+            "rating": rating,
+            "img_url": img_url
+        })
+        sleep(0.1)
+
+    # 해당 요일에서 모은 웹툰들을 저장한 webtoon_dict를 전체 리스트에 추가
+    count = len(webtoon_dict["webtoon_items"])
+    print(f"{menu.text}요일: {count}건 크롤링 완료")
+    webtoon_list.append(webtoon_dict)
+
+driver.quit()
 
